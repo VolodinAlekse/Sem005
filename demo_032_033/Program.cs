34431b0dae11a1f2be14a0d997adf9936499b9c6
VolodinAlekse <volodin.8989@mail.ru>
2023-02-13 00:48:58 +0300

    demo_033

--- demo_032_033/Program.cs
+++ demo_032_033/Program.cs
@@ -7,6 +7,51 @@
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // -3; массив [6, 7, 19, 345, 3] -> да
 
+//Задача 32:
+
+// int[] GenerateArray(int size, int leftRange, int rightRange)
+// {
+//     int[] array = new int[size];
+//     var rand = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = rand.Next(leftRange, rightRange + 1);
+//     }
+//     return array;
+// }
+
+// void ChangePositiveAndNegative(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = -array[i];
+//     }
+// }
+
+// void PrintArray(int[] array)
+// {
+//     System.Console.WriteLine("[" + string.Join(",", array) + "]");
+// }
+
+// int[] myArray = GenerateArray(6, -9, 9);
+// PrintArray(myArray);
+// ChangePositiveAndNegative(myArray);
+// PrintArray(myArray);
+
+
+
+
+
+
+
+
+
+
+// Задача 33:
+
+
+
+
 int[] GenerateArray(int size, int leftRange, int rightRange)
 {
     int[] array = new int[size];
@@ -18,20 +63,28 @@ int[] GenerateArray(int size, int leftRange, int rightRange)
     return array;
 }
 
-void ChangePositiveAndNegative(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = -array[i];
-    }
-}
-
 void PrintArray(int[] array)
 {
     System.Console.WriteLine("[" + string.Join(",", array) + "]");
 }
 
+void FindNumber(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number)
+        {
+            System.Console.WriteLine("Такой элемент есть");
+            return;
+        }
+    }
+    System.Console.WriteLine("Такого элемента нет");
+}
+
 int[] myArray = GenerateArray(6, -9, 9);
+
+int number = 7;
+
 PrintArray(myArray);
-ChangePositiveAndNegative(myArray);
-PrintArray(myArray);
+
+FindNumber(myArray, number);
